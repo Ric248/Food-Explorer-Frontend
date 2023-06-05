e7c212e2f1985775d73ecfca8a4166551e0a298b
Ric248 <ric248@gmail.com>
2023-06-05 15:30:26 -0300

    [fix] Ajustando o design

--- src/components/ButtonText/styles.js
+++ src/components/ButtonText/styles.js
@@ -1,16 +1,17 @@
 import styled from "styled-components";
 
 export const Container = styled.button`
-    display: flex;
-    align-items: center;
-    text-align: end;
-    
-    font-size: 1.6rem;
-    font-family: 'Poppins', sans-serif;
-    
-    border: none;
-    gap: 0.8rem;
+  display: flex;
+  align-items: center;
+  text-align: end;
+  
+  font-size: 2.4rem;
+  font-family: 'Poppins', sans-serif;
+  font-weight: 700;
+  
+  border: none;
+  gap: 0.8rem;
 
-    background: none;
-    color: ${({ theme }) => theme.COLORS.BLUE};
+  background: none;
+  color: ${({ theme }) => theme.COLORS.WHITE_100};
 `;
