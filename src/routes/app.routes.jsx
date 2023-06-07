512216ee30af65c0af510197ff4155e76c55886b
Ric248 <ric248@gmail.com>
2023-06-07 16:24:42 -0300

    [fix] Corrigindo o sistema de adição de pratos

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,7 +1,8 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 
-import { Home } from "../pages/Home";
-import { Details } from '../pages/Details'
+import { Home } from '../pages/Home';
+import { Details } from '../pages/Details';
+import { Edit } from '../pages/Edit';
 import { AddDish } from '../pages/AddDish';
 
 export function AppRoutes() {
@@ -9,6 +10,7 @@ export function AppRoutes() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/details/:id" element={<Details />} />
+      <Route path="/edit/:id" element={<Edit />} />
       <Route path="/addDish" element={<AddDish />} />
     </Routes>
   )
